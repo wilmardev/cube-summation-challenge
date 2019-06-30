0f819970e31a2629fe37cbd989a89ac4e55b4c13
Wilmar Duque <wsduque@WSDUQUE-PC>
2019-06-30 18:35:11 -0500

    Se agrega capa de presentación que contiene webApi y tu proyecto pruebas

---
+++ Fuentes/Back-End/WebApi/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Challenge.CubeSummationNS.Model.Resources;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Challenge.CodeSummation.WebApi.Controllers
+{
+    public class BaseController : ApiController
+    {
+        protected HttpResponseMessage GenerarMensajeRespuesta(HttpStatusCode statusCode, object informacion = null)
+        {
+            return Request.CreateResponse(statusCode, informacion);
+        }
+
+        protected HttpResponseMessage ManejarExcepcion(Exception ex)
+        {
+            //Se grabaría en event log la excepcion y se retorna
+            //un mensaje genérico de error
+            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError,
+                CubeSummationResources.Error_Mensaje_Default);
+        }
+    }
+}
---
+++ Fuentes/Back-End/WebApi/Controllers/CubeSummationController.cs
@@ -0,0 +1,37 @@
+﻿using Challenge.CubeSummationNS.BR;
+using Challenge.CubeSummationNS.Model.Resources;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Challenge.CodeSummation.WebApi.Controllers
+{
+    public class CubeSummationController : BaseController
+    {
+        private readonly Factory factory = new Factory();
+
+        [HttpPost]
+        public HttpResponseMessage ProcesarInformacion(string datosCubos)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(datosCubos))
+                    return GenerarMensajeRespuesta(HttpStatusCode.OK, CubeSummationResources.Error_Datos_Entrada_Vacio);
+                ICubeSummationBR cubeSummation = ConstruirCubeSummationBR();
+                var respuesta = cubeSummation.ProcesarInformacion(datosCubos);
+                return GenerarMensajeRespuesta(HttpStatusCode.OK, respuesta);
+            }
+            catch (Exception ex)
+            {
+                return ManejarExcepcion(ex);
+            }
+        }
+
+        private ICubeSummationBR ConstruirCubeSummationBR()
+        {
+            //Se tomarían variables de HttpSession, origen, usuario para trazabilidad
+            return factory.ContruirCubeSummationBR();
+        }
+    }
+}
